9120b0e7d539eac797a907b633fd4722ca6e8db9
maximel77 <maxime.laville7732@gmail.com>
2025-02-21 11:52:16 +0100

    Update SolutionTP4.py

--- SolutionTP4.py
+++ SolutionTP4.py
@@ -1,1 +1,21 @@
 
+import numpy as np
+from PIL import Image
+def see_gaussian(seuil):
+    image= Image.open("gaussian.png")
+    n= np.asarray(image)
+    
+    rouge_8= n[:,:,0].astype(np.uint8)
+ 
+    im=np.zeros(image.size)
+    compteur=0
+    for i in range(image.size[0]):
+        for j in range(image.size[0]):
+           if rouge_8[i][j]>seuil:
+               im[i][j]=1
+               compteur+=1
+    
+    return im,compteur,compteur/(image.size[0]*image.size[1])
+
+
+    
